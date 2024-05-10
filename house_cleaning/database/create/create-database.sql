d57d0cbcbc82e29a63edce3ccf11fef19659f55b
lucas-neitzke-moderniza <lucas.neitzke@alunos.bpkedu.com.br>
2024-05-09 21:46:35 -0300

    atualiza database

--- house_cleaning/database/create/create-database.sql
+++ house_cleaning/database/create/create-database.sql
@@ -1,4 +1,3 @@
-/* CREATE */
 CREATE TABLE faxineira (
     id_faxineira INT PRIMARY NOT NULL,
     email VARCHAR(255) NOT NULL,
@@ -6,24 +5,3 @@ CREATE TABLE faxineira (
     sobrenome VARCHAR(255), 
     cpf VARCHAR(14),
 );
-
-
-/* DELETE */
-DELETE FROM faxineira WHERE id_faxineira > 0;
-
-/* READ */
-SELECT * FROM faxineira
-
-/* INSERT */
-INSERT INTO 
-    faxineira (nome, sobrenome, cpf)
-VALUES 
-    ('', '', '');
-
-/* UPDATE */
-UPDATE faxineira
-    SET 
-        nome = '', 
-        sobrenome = '', 
-        cpf = ''
-WHERE id_faxineira > 0;
